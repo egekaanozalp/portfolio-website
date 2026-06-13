222f0fe2d466099bfba8624b55da0fa9ffa4d818
egekaanozalp <egekaan@sabanciuniv.edu>
2026-06-13 21:33:24 +0300

    feat: serve /favicon.ico and /robots.txt routes to satisfy Google search requirements.

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,10 +2,23 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from general.models import GeneralSettings
+
+
+def favicon_redirect(request):
+    general = GeneralSettings.get()
+    if general.favicon:
+        return HttpResponseRedirect(general.favicon.url)
+    return HttpResponseRedirect(settings.STATIC_URL + "img/favicon.png")
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("favicon.ico", favicon_redirect),
+    path("robots.txt", lambda request: HttpResponse(
+        "User-agent: *\nAllow: /\n", content_type="text/plain"
+    )),
     path("ping", lambda request: HttpResponse("ok", content_type="text/plain"), name="ping"),
     path("", include("core.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
